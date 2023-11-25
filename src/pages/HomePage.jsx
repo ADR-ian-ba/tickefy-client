1f930655a3a8fc704f69434fa46354a40d1ad4c9
Adrian Purnama <adrianpurnama209@gmail.com>
2023-11-25 12:49:59 +0700

    added loading

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Footer, Navigation, RenderCard } from "../components"
 import { UserContext } from "../context/UserContext"
 import { Link } from "react-router-dom"
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom"
 const HomePage = () => {
 
   const {recomendation, setRecomendation, isLogin} = useContext(UserContext)
+  const[loading, setLoading] = useState(true)
+  const [videoPlaying, setVideoPlaying] = useState(true);
 
   useEffect(() => {
          fetch("http://localhost:4000/data", {
@@ -26,12 +28,32 @@ const HomePage = () => {
          })
          .catch(error => {
              console.error('Error:', error);
+         })
+         .finally(()=>{
+          setLoading(false)
          });
      }, []);
  
      useEffect(()=>{
-       console.log(recomendation)
+
      },[recomendation])
+
+     const handleVideoEnd = () => {
+      setVideoPlaying(false);
+  };
+
+    if(loading || videoPlaying){
+    return (
+      <div>
+        <p>video is played</p>
+        <video onEnded={handleVideoEnd} autoPlay muted style={{widows:"100%", height:"100%"}}>
+          <source src="../public/assets/Tickefy.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+
+    )
+    }
   
   return (
     <section className="home">
