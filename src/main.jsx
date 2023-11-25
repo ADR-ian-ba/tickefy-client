a5ff5a44d8651727473551f13a007d868e94a525
Adrian Purnama <adrianpurnama209@gmail.com>
2023-11-25 17:18:04 +0700

    test commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { UserContextProvider } from './context/UserContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <BrowserRouter>
       <UserContextProvider>
         <App />
         </UserContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
 )
